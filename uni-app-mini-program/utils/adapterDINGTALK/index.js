4e902a081698a724cc7db096353d47973cc6bcc5
kai Wu <ioswukai@163.com>
2023-05-27 15:40:15 +0800

    MP-DINGTALK 条件编译有大坑，使用MP-WEIXIN做条件编译

--- uni-app-mini-program/utils/adapterDINGTALK/index.js
+++ uni-app-mini-program/utils/adapterDINGTALK/index.js
@@ -1,5 +1,11 @@
 /**
  * 为了适配兼容钉钉小程序，做的条件判断
+ * !
+ * !!!!!不知道为什么 MP-DINGTALK的环境变量没有生成，
+ *      导致#ifndef  MP-DINGTALK，和#ifdef MP-DINGTALK里的代码块
+ *      都不会被打包编译进unpackage/dist/dev或build的目标文件中
+ * 		这里就使用微信平台来判断了
+ *  !!!!!
  */
 
 export default {
@@ -7,13 +13,13 @@ export default {
 	 * 网络请求
 	 */
 	request: (obj) => {
-		// #ifndef MP-DINGTALK
-		// 非钉钉
+		// #ifdef MP-WEIXIN
+		// 微信
 		 uni.request(obj);
 		// #endif
 		
-		// #ifdef APP-DINGTALK
-		// 钉钉
+		// #ifndef MP-WEIXIN
+		// 非微信，即钉钉
 		dd.httpRequest(obj);
 		// #endif
 	},
@@ -21,12 +27,12 @@ export default {
 	 * 网络请求的配置对象
 	 */
 	httpConfig: (config) => {
-		// #ifdef MP-DINGTALK
-		// 钉钉
+		// #ifndef MP-WEIXIN
+		// 非微信，即钉钉
 		// Content-Type为application/x-www-form-urlencoded即默认的接口请求方式
 		// 当Content-Type为application/json时，data参数只支持json字符串，
 		// 用户需要手动调用JSON.stringify进行序列化 data: JSON.stringify({ from: '钉钉', production: 'DingTalk',}),
-		// 钉钉小程序只支持headers，清空header
+		// 非微信，即钉钉小程序只支持headers，清空header
 		config.headers = config.header;
 		config.header = {};
 		// 数据类型为json
@@ -39,13 +45,13 @@ export default {
 	 * toast内容
 	 */
 	showToast: (obj) => {
-		// #ifndef MP-DINGTALK
-		// 非钉钉
+		// #ifdef MP-WEIXIN
+		// 微信
 		uni.showToast(obj);
 		// #endif
 		
-		// #ifdef APP-DINGTALK
-		// 钉钉
+		// #ifndef MP-WEIXIN
+		// 非微信，即钉钉
 		// 没有position参数，content就是title参数
 		obj.position = null;
 		obj.content = obj.title;
