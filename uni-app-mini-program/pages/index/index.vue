6dd92a9d9f94fbcd2967e629dde508081bbfce57
kai Wu <ioswukai@163.com>
2023-07-10 11:21:43 +0800

    联调一些新接口

--- uni-app-mini-program/pages/index/index.vue
+++ uni-app-mini-program/pages/index/index.vue
@@ -29,7 +29,7 @@ import network from '../../utils/globalConfig/network.js';
 				// 	c: 'search', 
 				// 	// 公告资讯的栏目id
 				// 	catid: '13', 
-				// 	api_call_function: 'category_list',
+				// 	api_call_function: 'news_category_content_list',
 				// };
 				
 				// 新闻资讯 列表
@@ -37,18 +37,93 @@ import network from '../../utils/globalConfig/network.js';
 				// 	s: 'news', 
 				// 	c: 'search', 
 				// 	// 新闻资讯的栏目id
-				// 	catid: '42',
+				// 	catid: '17',
+				// 	page: 1,
+				// 	pagesize: network.pagesize,
+				// 	api_call_function: 'news_category_content_list',
+				// };
+				
+				// 亲亲讲堂--文博知识 列表
+				// const data = {
+				// 	s: 'news', 
+				// 	c: 'search', 
+				// 	// 文博知识的栏目id
+				// 	catid: '5',
+				// 	page: 1,
+				// 	pagesize: network.pagesize,
+				// 	api_call_function: 'news_category_content_list',
+				// };
+				
+				// 亲亲讲堂--学生作品 列表
+				// const data = {
+				// 	s: 'news', 
+				// 	c: 'search', 
+				// 	// 学生作品的栏目id
+				// 	catid: '6',
+				// 	page: 1,
+				// 	pagesize: network.pagesize,
+				// 	api_call_function: 'news_category_content_list',
+				// };
+				
+				// 亲亲讲堂--文博讲堂 列表
+				// const data = {
+				// 	s: 'news', 
+				// 	c: 'search', 
+				// 	// 文博讲堂的栏目id
+				// 	catid: '2',
+				// 	page: 1,
+				// 	pagesize: network.pagesize,
+				// 	api_call_function: 'news_category_content_list',
+				// };
+				
+				// 常见问答 列表
+				// const data = {
+				// 	s: 'news', 
+				// 	c: 'search', 
+				// 	// 常见问答的栏目id
+				// 	catid: '3',
+				// 	page: 1,
+				// 	pagesize: network.pagesize,
+				// 	api_call_function: 'news_category_content_list',
+				// // };
+				
+				// 互动问答-栏目分类 列表
+				// const data = {
+				// 	s: 'wenda',
+				// 	c: 'search', 
+				// 	// 互动问答的栏目id
+				// 	catid: '14',
 				// 	page: 1,
 				// 	pagesize: network.pagesize,
 				// 	api_call_function: 'category_list',
 				// };
 				
-				// 详情页
+				// 互动问答-各子栏目 列表
+				// const data = {
+				// 	s: 'wenda', 
+				// 	c: 'search', 
+				// 	// 互动问答-子栏目id
+				// 	catid: '15',
+				// 	page: 1,
+				// 	pagesize: network.pagesize,
+				// 	api_call_function: 'wenda_category_content_list',
+				// };
+				
+				// 文章详情页
+				// const data = { 
+				// 	c: 'show', 
+				// 	// 文章内容id
+				// 	id: '25', 
+				// 	api_call_function: 'news_content_detail'
+				// }
+				
+				// 问答详情页
 				const data = { 
 					c: 'show', 
-					// 文章id
-					id: '28', 
-					api_call_function: 'content_detail'}
+					// 问答内容id
+					id: '25', 
+					api_call_function: 'wenda_content_detail',
+				}
 				
 				
 				const res = await request({data});
